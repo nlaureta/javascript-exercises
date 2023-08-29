6f8e749d69b49ce9b8fe6d8442a25fcfaee16146
Nathan L <73408370+nlaureta@users.noreply.github.com>
2023-08-28 22:51:01 -0700

    Completed exercies
    helloWord, repeatString, reverseString, removeFromArray, sumAll, leapYears, and tempConversion completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...argument) {
+    for (let i = 0; i < argument.length; i++) {
+        let index = array.indexOf(argument[i]);
 
+        if (index !== -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let conversion = 0;
+  conversion = (fahrenheit - 32) * 5/9;
+  return Math.round(conversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let conversion = 0;
+  conversion = (celsius * 9/5) + 32;
+  return Math.round(conversion * 10) / 10; 
 };
 
 // Do not edit below this line
